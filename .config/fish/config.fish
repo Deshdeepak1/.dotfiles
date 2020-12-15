1be8959e9bce8e1cbc680041d02f8d9de2f1c71a
Deshdeepak <rkdeshdeepak1@gmail.com>
2020-12-15 09:29:20 +0530

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+# Abbreviations
+abbr 	vrc  	"vim ~/.vimrc"
+abbr 	nvrc 	"vim ~/.config/nvim/init.vim"
+abbr	frc 	"vim ~/.config/fish/config.fish"
+
+# Dot files management
+alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
