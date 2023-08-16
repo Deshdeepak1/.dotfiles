98dcb2fa8d10cd2b5d04c85dc6c1e32628ced96f
Deshdeepak <rkdeshdeepak1@gmail.com>
2023-08-17 02:39:51 +0530

    fish: update config -> check conditions, interactive; start x -> sx

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,50 @@
-# Programs Recommended:
-# fish, vim/nvim/nvim-nightly, pynvim/pyvim, exa, tmux, git, screenfetch, bat, python(pip - pylint, autopep8),
-# Hack font, noto-fonts-emoji, fd, fzf, lazygit, ranger, ranger_devicons, ctags, ripgrep, luarocks
-# awesomewm, dmenu-jacob-git, termite, figlet, lolcat, cowsay, fortune, asciiquarium
-# dragon-drag-and-drop, strace, screenkey
+set fish_greeting
+
+fish_add_path -p ~/.local/scripts ~/.local/bin
+set fish_cursor_unknown block
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        sx 2> /dev/null
+    end
+end
+
+
+if status is-interactive
+    if ! test -n "$TMUX"
+        # OFF by default: shell editor wm de palette
+        PF_INFO="ascii title os host kernel uptime pkgs memory wm" pfetch
+        #neofetch
+    end
+end
+
+# Git
 
 # git config --global user.email "rkdeshdeepak1@gmail.com"
 # git config --global user.name "Deshdeepak"
 # git config --global credential.helper cache/store
-set fish_greeting
 
 # Dot files management
-alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+alias dotfiles="git --git-dir=$HOME/.dotfiles"
+#dotfiles remote add https://gitea.deshdeepak.xyz/deshdeepak/.dotfiles
+#dotfiles pull -u origin master
 #dotfiles config status.showUntrackedFiles no
 #dotfiles config --local credential.helper cache/store
 #git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local core.worktree $HOME
-alias dvim="env GIT_DIR=$HOME/.dotfiles GIT_WORK_TREE=$HOME vim"
+abbr dv "GIT_DIR=~/.dotfiles $EDITOR"
 
 # Abbreviations
-# abbr    vrc     "dvim ~/.vimrc"
-abbr    nvrc    "cd ~/.config/nvim/ ; dvim ~/.config/nvim/init.lua"
-abbr    frc     "cd ~/.config/fish/ ; dvim ~/.config/fish/config.fish"
-abbr    awrc    "cd ~/.config/awesome/ ; dvim ~/.config/awesome/rc.lua"
 abbr    fpac    "pacman -Slq | fzf -m --preview 'cat (pacman -Si {1} | psub) (pacman -Fl {1} | awk \"{print \$2}\" | psub)' | xargs -ro sudo pacman -Sy"
 abbr    fyay    "yay -Slq | fzf -m --preview 'cat (yay -Si {1} | psub) (yay -Fl {1} | awk \"{print \$2}\" | psub)' | xargs -ro  yay -Sy"
 abbr    fparu   "paru -Slq | fzf -m --preview 'cat (paru -Si {1} | psub) (paru -Fl {1} | awk \"{print \$2}\" | psub)' | xargs -ro  paru -Sy"
-abbr actv ". venv/bin/activate.fish"
-abbr acth ". ~/hvenv/bin/activate.fish"
+abbr av ". venv/bin/activate.fish"
+abbr ah ". ~/hvenv/bin/activate.fish"
 # abbr    fdur    "find . -iname \"*.mp4\" -exec ffprobe -v quiet -of csv=p=0 -show_entries format=duration {} \;  | paste -sd+ | xargs -I \"{}\" python -c \"t = {} ; h = t//3600; m = (t % 3600)// 60; s = t % 60; ms =(s * 1000) % 1000;  print('%d hours %d minutes %d seconds %d miliseconds' %(h, m, s, ms))\""
+abbr p "sudo pacman -Syyu"
+abbr v "$EDITOR"
+abbr n "$EDITOR"
 
 
 # Aliases
@@ -41,15 +59,11 @@ alias aria2cc="aria2c -c -s 16 -x 16 -k 1M -j 32  --file-allocation none"
 alias xsc="xclip -sel clipboard"
 # export SHELL="sh"
 
-
 export FZF_DEFAULT_COMMAND="fd --exclude '.git/' --hidden --type f"
 export FZF_CTRL_T_COMMAND="fd --exclude '.git/' --hidden --type f"
 export FZF_ALT_C_COMMAND="fd --exclude '.git/' --hidden --type d"
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
-set  PATH $HOME/.local/share/nvim/mason/bin $HOME/.local/bin $PATH
-set fish_cursor_unknown block
-export EDITOR="vim"
 
 function venv_act --on-variable PWD --description 'change directory - activate venv'
     # builtin cd $param $argv
@@ -64,12 +78,16 @@ function venv_act --on-variable PWD --description 'change directory - activate v
 
     # If venv is not activated or a different venv is activated and venv exist.
     if test "$VIRTUAL_ENV" != "$venvdir" -a -f "$actvate_file"
-        . "$actvate_file"
+        source "$actvate_file"
     # If venv activated but the current (git) dir has no venv.
     # else if not test -z "$VIRTUAL_ENV" -o -e "$gitdir/.venv"
     #     deactivate
     end
 end
+
+# ffmpeg record
+# ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab  -i :1 -t 5 output.mp4
+# ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab  -i :1.0+100,200 -t 5 output.mp4 # 100,200
 
 
 
--- .config/fish/functions/codi.fish
+++
@@ -1,7 +0,0 @@
-#Codi
-function codi
-   nvim -c \
-     "let g:startify_disable_at_vimenter = 1 |\
-     hi CodiVirtualText guifg=cyan
-     Codi python" "$argv"
-end
