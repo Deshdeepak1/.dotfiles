b81f1d5a0ef2121b771e0bbbfd39f125e5e97edd
Deshdeepak <rkdeshdeepak1@gmail.com>
2025-02-03 14:16:17 +0530

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
 pkg update && pkg upgrade
 pkg install -y fish neovim openssh fzf fd ripgrep tmux bat dust \
 	jq ncdu
+ 
+git config --global user.email "rkdeshdeepak1@gmail.com"
+git config --global user.name "Deshdeepak"
+git config --global credential.helper store
 
 bat cache --build
+
+dotfiles remote add origin https://github.com/deshdeepak1/.dotfiles
+dotfiles config --local status.showUntrackedFiles no
+dotfiles config --local credential.helper store
+dotfiles fetch --set-upstream origin termux
+dotfiles reset --hard FETCH_HEAD
+dotfiles remote set-url origin git@github.com:Deshdeepak1/.dotfiles.git
