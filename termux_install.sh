f2132ddc7ddb5cae36663e5e0faa468e2c32b228
Deshdeepak <rkdeshdeepak1@gmail.com>
2025-02-03 12:49:02 +0530

    Update Mon Feb  3 12:49:02 IST 2025

--- termux_install.sh
+++ termux_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pkg update && pkg upgrade
-pkg install -y fish neovim fzf fd ripgrep tmux bat dust
+pkg install -y fish neovim openssh fzf fd ripgrep tmux bat dust \
+	jq ncdu
+
+bat cache --build
