4fc29ec5235bfd227f92ecddf38f6ff9346883f1
Deshdeepak <rkdeshdeepak1@gmail.com>
2025-02-03 15:08:15 +0530

    Create fish_user_key_bindings.fish

---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,7 @@
+function fish_user_key_bindings
+    fzf_key_bindings
+    fish_vi_key_bindings
+    bind -M insert -m default jk backward-char force-repaint 
+    bind -M insert \cb fzf_bcd_widget
+    bind -M insert \cp 'pwd|xsc'
+end
