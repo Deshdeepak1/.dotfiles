0d9220b029bbe72a0f0beac2291a338b14d2f5c9
Deshdeepak <rkdeshdeepak1@gmail.com>
2025-03-09 14:35:00 +0530

    Update projects picker

--- .config/nvim/lua/plugins/utils/snacks.lua
+++ .config/nvim/lua/plugins/utils/snacks.lua
@@ -1,3 +1,93 @@
+---@param item snacks.picker.Item
+local function format_filename(item, picker)
+  ---@type snacks.picker.Highlight[]
+  local ret = {}
+  if not item.file then
+    return ret
+  end
+  local path = Snacks.picker.util.path(item) or item.file
+  -- path = Snacks.picker.util.truncpath(path, picker.opts.formatters.file.truncate or 40, { cwd = picker:cwd() })
+  path = vim.fn.fnamemodify(path, ":~:.")
+  local name, cat = path, "file"
+  if item.buf and vim.api.nvim_buf_is_loaded(item.buf) then
+    name = vim.bo[item.buf].filetype
+    cat = "filetype"
+  elseif item.dir then
+    cat = "directory"
+  end
+
+  if picker.opts.icons.files.enabled ~= false then
+    local icon, hl = Snacks.util.icon(name, cat, {
+      fallback = picker.opts.icons.files,
+    })
+    if item.dir and item.open then
+      icon = picker.opts.icons.files.dir_open
+    end
+    icon = Snacks.picker.util.align(icon, picker.opts.formatters.file.icon_width or 2)
+    ret[#ret + 1] = { icon, hl, virtual = true }
+  end
+
+  local base_hl = item.dir and "SnacksPickerDirectory" or "SnacksPickerFile"
+  local function is(prop)
+    local it = item
+    while it do
+      if it[prop] then
+        return true
+      end
+      it = it.parent
+    end
+  end
+
+  if is("ignored") then
+    base_hl = "SnacksPickerPathIgnored"
+  elseif is("hidden") then
+    base_hl = "SnacksPickerPathHidden"
+  elseif item.filename_hl then
+    base_hl = item.filename_hl
+  end
+  local dir_hl = "SnacksPickerDir"
+
+  if picker.opts.formatters.file.filename_only then
+    path = vim.fn.fnamemodify(item.file, ":t")
+    ret[#ret + 1] = { path, base_hl, field = "file" }
+  else
+    local dir, base = path:match("^(.*)/(.+)$")
+    if base and dir then
+      if picker.opts.formatters.file.filename_first then
+        ret[#ret + 1] = { base, base_hl, field = "file" }
+        ret[#ret + 1] = { " " }
+        ret[#ret + 1] = { dir, dir_hl, field = "file" }
+      else
+        ret[#ret + 1] = { dir .. "/", dir_hl, field = "file" }
+        ret[#ret + 1] = { base, base_hl, field = "file" }
+      end
+    else
+      ret[#ret + 1] = { path, base_hl, field = "file" }
+    end
+  end
+  if item.pos and item.pos[1] > 0 then
+    ret[#ret + 1] = { ":", "SnacksPickerDelim" }
+    ret[#ret + 1] = { tostring(item.pos[1]), "SnacksPickerRow" }
+    if item.pos[2] > 0 then
+      ret[#ret + 1] = { ":", "SnacksPickerDelim" }
+      ret[#ret + 1] = { tostring(item.pos[2]), "SnacksPickerCol" }
+    end
+  end
+  ret[#ret + 1] = { " " }
+  if item.type == "link" then
+    local real = uv.fs_realpath(item.file)
+    local broken = not real
+    real = real or uv.fs_readlink(item.file)
+    if real then
+      ret[#ret + 1] = { "-> ", "SnacksPickerDelim" }
+      ret[#ret + 1] =
+        { Snacks.picker.util.truncpath(real, 20), broken and "SnacksPickerLinkBroken" or "SnacksPickerLink" }
+      ret[#ret + 1] = { " " }
+    end
+  end
+  return ret
+end
+
 return {
   "folke/snacks.nvim",
   -- cond = false,
@@ -132,7 +222,19 @@ return {
     { "<leader>f:", function() Snacks.picker.commands() end, desc = "Commands" },
     { "<leader>fH", function() Snacks.picker.command_history() end, desc = "Command History" },
     { "<leader>fi", function() Snacks.picker.icons() end, desc = "Icons" },
-    { "<leader>fp", function() Snacks.picker.projects({  dev = { "~/labs/repo/" },}) end, desc = "Projects" },
+    -- stylua: ignore end
+    {
+      "<leader>fp",
+      function()
+        Snacks.picker.projects({
+          dev = { "~/labs/repo/" },
+          filter = { paths = { [vim.fn.stdpath("data")] = false } },
+          format = format_filename,
+        })
+      end,
+      desc = "Projects",
+    },
+    -- stylua: ignore start
     { "<leader>fn", function() Snacks.picker.notifications() end, desc = "Notification History" },
     { "<leader>fj", function() Snacks.picker.jumps() end, desc = "Jumps" },
     { "<leader>fq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
